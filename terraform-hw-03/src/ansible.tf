40156e6dc4101367345908057a18ee24db99f6cd
Torrmund <sanyaya.emelyanov@mail.ru>
2024-10-25 23:01:00 +0400

    fixing for hw-03 task 4.2

--- terraform-hw-03/src/ansible.tf
+++ terraform-hw-03/src/ansible.tf
@@ -4,7 +4,7 @@ resource "local_file" "hosts_templatefile" {
   {
     webservers = yandex_compute_instance.web,
     databases = yandex_compute_instance.db,
-    storage = yandex_compute_instance.storage
+    storage = [yandex_compute_instance.storage]
   }
   )
 
